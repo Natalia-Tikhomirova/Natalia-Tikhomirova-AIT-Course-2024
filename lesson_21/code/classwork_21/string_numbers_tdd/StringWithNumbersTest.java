6b33a9ae80c9bcade69daa801b36be9b9e6e2a40
Natalia Tikhomirova <ntikhomirova@gmail.com>
2024-10-10 10:53:05 +0200

    Project update

---
+++ lesson_21/code/classwork_21/string_numbers_tdd/StringWithNumbersTest.java
@@ -0,0 +1,31 @@
+package classwork_21.string_numbers_tdd;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringWithNumbersTest {
+
+    StringWithNumbers stringWithNumbers; // object для тестирования
+
+    @BeforeEach
+    void setUp() {
+        stringWithNumbers = new StringWithNumbers("123 321 10"); // 454
+    }
+
+    @Test
+    void sumOfNumbersInStringTest(){  // mini maid
+
+       // StringWithNumbers.sumOfNumbersInString() // можно так по имени класса, но убираем static
+
+        String str = "123 321 10"; // придумали строку и обработали в методе
+        int expected = 454;
+        int actual = stringWithNumbers.sumOfNumbersInString(str); //  результат actual
+        assertEquals(expected,actual); // метод из библиотеки: сравнивает ожидаемое и актуальное
+
+
+    }
+
+
+}
