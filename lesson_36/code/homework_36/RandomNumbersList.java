a13ac853ccfe415bef483199ba4f981faa3dd15a
Natalia Tikhomirova <ntikhomirova@gmail.com>
2024-10-31 23:13:10 +0100

    Project update

--- lesson_36/code/homework_36/RandomNumbersList.java
+++ lesson_36/code/homework_36/RandomNumbersList.java
@@ -10,17 +10,17 @@ public class RandomNumbersList {
         List<Integer> numbers = new ArrayList<>();
         Random random = new Random();
 
-        // Заполняем ArrayList 20 случайными числами от 1 до 10
+        // Заполняем ArrayList 20 случайными числами от 1 до 10:
         for (int i = 0; i < 20; i++) {
-            int randomNum = random.nextInt(10) + 1;  // Генерация числа от 1 до 10
+            int randomNum = random.nextInt(10) + 1;      // Генерация числа от 1 до 10
             numbers.add(randomNum);
         }
 
         System.out.println("Список с дубликатами: " + numbers);
 
-        // Удаляем дубликаты
+        // Удаляем дубликаты:
         Set<Integer> uniqueNumbers = new HashSet<>(numbers);  // Добавление всех элементов списка в Set
-        numbers = new ArrayList<>(uniqueNumbers);  // Преобразование Set обратно в ArrayList
+        numbers = new ArrayList<>(uniqueNumbers);             // Преобразование Set обратно в ArrayList
 
         System.out.println("Список без дубликатов: " + numbers);
     }
