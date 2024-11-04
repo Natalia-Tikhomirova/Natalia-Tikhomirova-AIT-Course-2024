56243f84bdb90dcff379794cc0b51b0074ce1df4
Natalia Tikhomirova <ntikhomirova@gmail.com>
2024-11-04 19:31:44 +0100

    Project update

---
+++ lesson_38/code/homework_38/model/GermanyAppl.java
@@ -0,0 +1,93 @@
+package homework_38.model;
+
+// В классе GermanyAppl cоздать LinkedList, содержащий города - столицы земель Германии.
+// Опробовать на нем методы, которые доступны для класса LinkedList.
+// Добавить вольный город Гамбург в начало, в середину списка, в конец списка.
+// Отсортировать список по алфавиту и в обратном порядке.
+
+import classwork_38.linked_list_test.LinkedListAppl;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class GermanyAppl {
+    public static void main(String[] args) {
+
+        LinkedList<String> germanStateCapitals = new LinkedList<>(Arrays.asList("Stuttgart", "München", "Berlin", "Potsdam", "Bremen", "Hamburg", "Wiesbaden", "Hannover", "Schwerin", "Düsseldorf", "Mainz", "Saarbrücken", "Dresden", "Magdeburg", "Kiel", "Erfurt"));
+
+        printGermanStateCapitals(germanStateCapitals);
+
+        System.out.println();
+        System.out.println("________________________________");
+        System.out.println(germanStateCapitals.size());
+        System.out.println("----------------------------------");
+        System.out.println(germanStateCapitals.getFirst());
+        System.out.println("----------------------------------");
+        System.out.println(germanStateCapitals.getLast());
+        System.out.println("----------------------------------");
+        System.out.println(germanStateCapitals.get(5)); // Hamburg
+        System.out.println("----------------------------------");
+        germanStateCapitals.set(1,"Hamburg"); // München ->  Hamburg
+        printGermanStateCapitals(germanStateCapitals);
+        System.out.println();
+        System.out.println("-----------------------------------------------------------------");
+
+        Iterator<String> iterator = germanStateCapitals.iterator(); // remove Hamburg
+        while (iterator.hasNext()) {
+            String city = iterator.next();
+            if (city.equals("Hamburg")) {
+                iterator.remove();
+            }
+        }
+        printGermanStateCapitals(germanStateCapitals);
+        System.out.println();
+        System.out.println("-----------------------------------------------------------------");
+
+
+        germanStateCapitals.remove(germanStateCapitals.indexOf("Bremen")); // remove Bremen
+        printGermanStateCapitals(germanStateCapitals);
+        System.out.println();
+        System.out.println("-----------------------------------------------------------------");
+
+        printGermanStateCapitals(germanStateCapitals);
+        System.out.println();
+
+        // Добавить вольный город Гамбург в начало
+        germanStateCapitals.addFirst("Hamburg");
+        System.out.println(germanStateCapitals);
+        System.out.println();
+        System.out.println("-----------------------------------------------------------------");
+
+        //  в середину списка
+        System.out.println(germanStateCapitals.size());
+        System.out.println("-----------------------------------------------------------------");
+        germanStateCapitals.add(germanStateCapitals.size()/ 2, "Hamburg");
+        System.out.println(germanStateCapitals);
+        System.out.println("-----------------------------------------------------------------");
+
+
+        // в конец списка.
+        germanStateCapitals.add("Hamburg");
+        System.out.println(germanStateCapitals);
+        System.out.println();
+
+
+
+
+
+
+
+
+
+
+    } // end of main
+
+    private static void  printGermanStateCapitals (LinkedList<String> germanStateCapitals){
+
+        for (String germanStateCapital : germanStateCapitals) {
+            System.out.print(germanStateCapital + " , ");
+        }
+
+    }
+} // end of class
